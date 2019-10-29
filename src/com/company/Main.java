56e9a52941e4bddbb73ce30fc948575c62678da4
Charlie <calhusen@gmail.com>
2019-10-29 15:14:48 -0400

    stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
         
     public static void main(String[] args) {
 	// write your code here
-
+        awards();
     }
     public static void awards(){
         Scanner userInput = new Scanner(System.in);
