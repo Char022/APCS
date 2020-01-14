f75444290106bdcbbfb8f0eb3d99a31cac13e623
Charlie <calhusen@gmail.com>
2020-01-14 14:42:15 -0500

    set up chapter three work branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         
     public static void main(String[] args) throws InterruptedException{
 	// write your code here
-        //multiplicationTable();
+        multiplicationTable();
         slotMachine();
         guessingGame();
     }
