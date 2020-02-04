ed2e1fc14af1238debfd544bad00dd4e59e566f6
Charlie <calhusen@gmail.com>
2020-02-04 14:37:47 -0500

    yeet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args){
 	// write your code here
+        powersOfTwo();
         Circle spot = new Circle(5);
         
         System.out.println("Circle radius: " + spot.getRadius());
@@ -17,5 +18,18 @@ public class Main {
         System.out.println("Circumference: " + spot.getCircumference());
         Circle.displayArea();
     }
-}
+
+    public static String questionNumberFour(String name, int number) {
+        String output = "Welcome " + name + "! You are visitor number " + (number + 1) + ".";
+        return output;
+    }
+
+    public static void powersOfTwo(){
+        int x = 2;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(x);
+            x *= 2;
+        }
+    }
+    }
 
