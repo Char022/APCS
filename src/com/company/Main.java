3f285f1fa86a8a52bd09e472a7291fd2c8fabf55
Charlie <calhusen@gmail.com>
2020-02-04 14:58:29 -0500

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,26 +10,40 @@ public class Main {
 
     public static void main(String[] args){
 	// write your code here
-        powersOfTwo();
-        Circle spot = new Circle(5);
-        
-        System.out.println("Circle radius: " + spot.getRadius());
-        System.out.println("Circle area: " +spot.area());
-        System.out.println("Circumference: " + spot.getCircumference());
-        Circle.displayArea();
+        Account acct1 = new Account("Ted Murphy", 72354, 102.56);
+        Account acct2 = new Account("Anita Gomez", 69713, 40.00);
+        Account acct3 = new Account("Sanchit Reddy", 93757, 759.32);
+
+        acct1.deposit(25.85);
+
+        double gomezBalance = acct2.deposit(500.00);
+        System.out.println("Gomez balance after deposit: " + gomezBalance);
+
+        System.out.println("Gomez balance after withdrawal: " +acct2.withdraw(430.75, 1.50));
+
+        acct3.withdraw(800.00, 0.0);
+
+        acct1.addInterest();
+        acct2.addInterest();
+        acct3.addInterest();
+
+        System.out.println();
+        System.out.println(acct1);
+        System.out.println(acct2);
+        System.out.println(acct3);
+
+        acct2 = acct1.transferFunds(acct2, 100);
+
+        System.out.println();
+        System.out.println(acct1);
+        System.out.println(acct2);
+        System.out.println(acct3);
+
+        Account acct4 = new Account("Person Four", 85311);
+
+        System.out.println();
+        System.out.println(acct4);
     }
 
-    public static String questionNumberFour(String name, int number) {
-        String output = "Welcome " + name + "! You are visitor number " + (number + 1) + ".";
-        return output;
-    }
-
-    public static void powersOfTwo(){
-        int x = 2;
-        for(int i = 1; i <= 10; i++){
-            System.out.println(x);
-            x *= 2;
-        }
-    }
-    }
+}
 
