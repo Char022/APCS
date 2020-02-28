59946a08c71d60bfe20eba4e52ca6d43a4725b1d
Charlie <calhusen@gmail.com>
2020-02-28 14:50:14 -0500

    countdown

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,20 @@ public class Main {
     public static void main(String[] args){
 	// write your code here
 
+    }
 
 
 
+
+    public static void countDown(int maxNumber){
+        if(maxNumber < 1){
+            System.out.println("Error");
+        }else{
+            for(int i = 0; i < maxNumber; i++){
+                System.out.println((maxNumber - i) + "...");
+            }
+        System.out.println("Blast off!!");
+        }
     }
 }
 
